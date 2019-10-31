25238acd5c20922caac52bfc57a5b12b5a82afd9
bryan villamil <bryan.v@sancrisoft.com>
2019-10-31 10:00:16 -0500

    add icons fontawesome

--- app/components/Carousel/index.js
+++ app/components/Carousel/index.js
@@ -6,10 +6,16 @@
 
 import React from 'react';
 import Slider from 'react-slick';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 // import { FormattedMessage } from 'react-intl';
 // import messages from './messages';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faChevronLeft,
+  faChevronRight,
+} from '@fortawesome/free-solid-svg-icons';
 
 import { CarouselHome } from '../../data/home';
 
@@ -19,6 +25,12 @@ import './slick-theme.css';
 import { ItemSlider, Img } from './styledComponents';
 
 function Carousel() {
+  const SlickButtonFix = ({ currentSlide, slideCount, children, ...props }) => (
+    <button type="button" {...props}>
+      {children}
+    </button>
+  );
+
   const settings = {
     dots: true,
     infinite: true,
@@ -26,6 +38,17 @@ function Carousel() {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
+    nav: true,
+    nextArrow: (
+      <SlickButtonFix>
+        <FontAwesomeIcon className="next" icon={faChevronRight} />
+      </SlickButtonFix>
+    ),
+    prevArrow: (
+      <SlickButtonFix>
+        <FontAwesomeIcon className="prev" icon={faChevronLeft} />
+      </SlickButtonFix>
+    ),
   };
 
   return (
@@ -39,6 +62,10 @@ function Carousel() {
   );
 }
 
-Carousel.propTypes = {};
+Carousel.propTypes = {
+  currentSlide: PropTypes.func.isRequired,
+  slideCount: PropTypes.func.isRequired,
+  children: PropTypes.object.isRequired,
+};
 
 export default Carousel;
--- app/containers/HomePage/styledComponents.js
+++ app/containers/HomePage/styledComponents.js
@@ -5,6 +5,7 @@ export const PageHome = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
+  flex-wrap: wrap;
 `;
 
 export const ContentCarousel = styled.div`
